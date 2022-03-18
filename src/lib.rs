f94e22d51affcbe794b4731c6b7a28ecebf8fd6f
Zeyla Hellyer <zeyla@hellyer.dev>
2022-03-17 21:19:17 -0400

    docs: add crate level docs
    Signed-off-by: Zeyla Hellyer <zeyla@hellyer.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,39 @@
+//! # twilight-cloudflare-workers
+//!
+//! Verify Discord interactions on Cloudflare Workers with Twilight.
+//!
+//! ### API
+//!
+//! The primary function in the API is `process`. It takes a Worker request and
+//! your application's public key for Discord, verifies the request signature to
+//! ensure it's from Discord, and deserializes the  request body as an interaction.
+//! Using it looks like this:
+//!
+//! ```rust
+//! let key = "discord public key from environment";
+//!
+//! let interaction = match twilight_cf_worker::process(&mut request, key) {
+//!     Ok(interaction) => interaction,
+//!     Err(source) => {
+//!         // Return the error as a Worker response.
+//!         return source.response();
+//!     }
+//! };
+//!
+//! // work with the interaction..
+//! ```
+//!
+//! The other function in the API is `response`, which takes an interaction response
+//! and produces a Worker response:
+//!
+//! ```rust
+//! return twilight_cf_worker::response(interaction_response);
+//! ```
+//!
+//! ### License
+//!
+//! ISC.
+
 #![deny(
     clippy::all,
     clippy::missing_const_for_fn,
